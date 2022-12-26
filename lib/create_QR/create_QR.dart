7c87530d303b11d6c6edf79a69ecd13cf7f5a54c
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2022-12-27 03:48:22 +0900

    change qrLink model 置き換え

--- lib/create_QR/create_QR.dart
+++ lib/create_QR/create_QR.dart
@@ -4,6 +4,7 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:provider/provider.dart';
 
 import 'package:qr_flutter/qr_flutter.dart';
@@ -13,7 +14,7 @@ import 'package:path_provider/path_provider.dart';
 import '../change_QR/change_QR_model.dart';
 import '../setting.dart';
 
-class CreateQRCode extends StatelessWidget {
+class CreateQRCode extends ConsumerWidget {
   final GlobalKey _shareKey = GlobalKey();
 
   String? gotCommunity;
@@ -22,83 +23,79 @@ class CreateQRCode extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider<QRChangeModel>(
-      create: (_) => QRChangeModel(gotCommunity)..fechInstitute(),
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            'QRコード表示',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: const Color.fromARGB(255, 67, 176, 190),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final qrChangeModel = ref.watch(qrChangeModelProvider);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'QRコード表示',
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
-        body: Center(
-          child: Consumer<QRChangeModel>(builder: (context, model, child) {
-            return Stack(
+        backgroundColor: const Color.fromARGB(255, 67, 176, 190),
+      ),
+      body: Center(
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    qrCode(model.QRLink, _shareKey),
-                    const SizedBox(height: 10.0),
-                    const Text(
-                      "QR Code",
-                      style: TextStyle(
-                        fontSize: 18,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                    Text(
-                      model.QRLink ?? "No QRCode Link",
-                      style: const TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 15.0),
-                    SizedBox(
-                      width: 150,
-                      height: 50,
-                      child: ElevatedButton(
-                          child: const Text(
-                            '共有する',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.grey,
-                            onPrimary: Colors.black,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          onPressed: () {
-                            if (model.QRLink != null) {
-                              shareImageAndText('sample_widget', _shareKey);
-                            } else {
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return const ErrorModal(
-                                      error_message:
-                                          "QRコードを認識できませんでした\nもう一度読み込んでください");
-                                },
-                              );
-                            }
-                          }),
-                    ),
-                  ],
-                ),
-                if (model.isLoading)
-                  Container(
-                    color: Colors.black54,
-                    child: const Center(
-                      child: CircularProgressIndicator(),
-                    ),
+                qrCode(qrChangeModel.qrLink, _shareKey),
+                const SizedBox(height: 10.0),
+                const Text(
+                  "QR Code",
+                  style: TextStyle(
+                    fontSize: 18,
+                    decoration: TextDecoration.underline,
                   ),
+                ),
+                Text(
+                  qrChangeModel.qrLink ?? "No QRCode Link",
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 15.0),
+                SizedBox(
+                  width: 150,
+                  height: 50,
+                  child: ElevatedButton(
+                      child: const Text(
+                        '共有する',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey,
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {
+                        if (qrChangeModel.qrLink != null) {
+                          shareImageAndText('sample_widget', _shareKey);
+                        } else {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const ErrorModal(
+                                  error_message:
+                                      "QRコードを認識できませんでした\nもう一度読み込んでください");
+                            },
+                          );
+                        }
+                      }),
+                ),
               ],
-            );
-          }),
+            ),
+            if (qrChangeModel.isLoading)
+              Container(
+                color: Colors.black54,
+                child: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+          ],
         ),
       ),
     );
